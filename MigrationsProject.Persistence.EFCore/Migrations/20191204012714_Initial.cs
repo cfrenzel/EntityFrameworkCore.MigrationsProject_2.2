2ea203f87827cd06632c012c09ea0ef673bdfe75
cfrenzel <camron.frenzel@bleepworks.com>
2019-12-03 21:22:50 -0500

    Initial Commit

---
+++ MigrationsProject.Persistence.EFCore/Migrations/20191204012714_Initial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MigrationsProject.Persistence.EFCore.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Number = table.Column<string>(maxLength: 50, nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
---
+++ MigrationsProject.Persistence.EFCore/README.txt
@@ -0,0 +1,20 @@
+﻿Our goal is to
+- keep the DbContext and the migrations in their own assembly
+- run the migrations as part of the deployment script rather than any specific webapp
+
+In order to use it set startup project to your MigrationHelper Project
+
+	- Messaging.Samples.Persistence.EFCore.MigrationHelper
+
+From the Package Manager Console
+
+    - Set DefaultProject to Persistence.EFCore
+	
+To Add a Migration
+
+	- Add-Migration Intitial
+
+To Update the database
+
+	- Update-Databae
+	
